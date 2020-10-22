820fc8f7d7f163e3fbf300b88dd968a97f1e4f3e
datpt <thanhdatkma96@gmail.com>
2020-10-22 14:54:35 +0700

    datpt - test

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! - DATPT"
+    return "Hello World! - DATPT123"
 
 if __name__ == "__main__":
     application.run()
